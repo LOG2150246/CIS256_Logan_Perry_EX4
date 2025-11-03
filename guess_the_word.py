72637869a500c975664cb25bcd25d459aa6a7409
Logan Perry <log2150246@maricopa.edu>
2025-11-02 22:06:40 -0700

    main loop framework works

--- guess_the_word.py
+++ guess_the_word.py
@@ -6,7 +6,6 @@ Create a word-guessing game where the user guesses a word letter by letter.
 You will use Git for version control, pytest for testing, and share your repository on GitHub.
 """
 import random
-from random import randint
 
 #list of words
 words=[
@@ -15,12 +14,32 @@ words=[
 "apple"
 ]
 
+attempts =10
+
 #Select a random word from a predefined list
 word=(random.choice(words))
+#for quick testing prints the word
 print(word)
+
 #Prompt the user to guess one letter at a time.
 
+
 #Reveal letters if the guess is correct; indicate if incorrect.
+correct_list=[]
+wrong_list=[]
+
+while True:
+    print(f"{attempts} attempts left")
+    guess = input("What letter would you like to guess\n:")
+
+    if guess in word:
+        correct_list=[].append(guess)
+        print(f"{guess} is in word")
+
+    else:
+        print(f"{guess} is not in the word")
+        wrong_list = [].append(guess)
+    attempts-=1
 
 #Continue until the user guesses the word or runs out of attempts.
 
