f041ef1418edd7dccb85c53e20638a9aa391deb7
Logan Perry <log2150246@maricopa.edu>
2025-11-03 12:15:34 -0700

    turned word_picker into a function lowered attempts to 3 for quick testing added if __name=main... to main loop got test_predefined_list working

--- guess_the_word.py
+++ guess_the_word.py
@@ -20,59 +20,64 @@ words=[
 "food"
 ]
 
-attempts =10
+attempts =3
 
 #Select a random word from a predefined list
-word=(random.choice(words))
-#for quick testing prints the word
-print(word)
+def word_picker(input_list):
+    picked_word = (random.choice(input_list))
+    return picked_word
+if __name__ == "__main__":
+    word=word_picker(words)
 
-#ini lists
-correct_list=[]
-wrong_list=[]
+    #for quick testing prints the word
+    print(word)
 
-while True:
+    #ini lists
+    correct_list=[]
+    wrong_list=[]
 
-    #loose condition and break
-    if attempts ==0:
-        print (f"sorry the word was {word} better luck next time!")
-        break
+    while True:
 
-    #prints how many attempts left
-    print(f"\nyou have {attempts} attempts to guess the hidden word")
+        #loose condition and break
+        if attempts ==0:
+            print (f"sorry the word was {word} better luck next time!")
+            break
 
-    #prints known letters
-    correct_count = 0
-    for letter in word:
-        if letter in correct_list:
-            print (f"{letter}", end='')
-            correct_count+=1
+        #prints how many attempts left
+        print(f"\nyou have {attempts} attempts to guess the hidden word")
+
+        #prints known letters
+        correct_count = 0
+        for letter in word:
+            if letter in correct_list:
+                print (f"{letter}", end='')
+                correct_count+=1
+            else:
+                print("_", end='')
+        print("")
+
+        #win condition and break
+        if correct_count == len(word):
+            print ("YOU WON!")
+            break
+
+        #gets input
+        guess = input("What letter would you like to guess\n:").lower().strip()
+
+        #if user put more than 1 letter in remind them only 1 letter
+        if len(guess) != 1:
+            print ("Sorry guess must be 1 letter")
+
+        #if guess was already guessed remind them
+        elif guess in correct_list or guess in wrong_list:
+            print (f"Sorry you already guessed {guess}")
+
+        #if guess in word...
+        elif guess in word:
+            correct_list.append(guess)
+
+        #else (guess is not in word)...
         else:
-            print("_", end='')
-    print("")
-
-    #win condition and break
-    if correct_count == len(word):
-        print ("YOU WON!")
-        break
-
-    #gets input
-    guess = input("What letter would you like to guess\n:").lower().strip()
-
-    #if user put more than 1 letter in remind them only 1 letter
-    if len(guess) != 1:
-        print ("Sorry guess must be 1 letter")
-
-    #if guess was already guessed remind them
-    elif guess in correct_list or guess in wrong_list:
-        print (f"Sorry you already guessed {guess}")
-
-    #if guess in word...
-    elif guess in word:
-        correct_list.append(guess)
-
-    #else (guess is not in word)...
-    else:
-        print(f"{guess} is not in the word")
-        wrong_list.append(guess)
-        attempts-=1
+            print(f"{guess} is not in the word")
+            wrong_list.append(guess)
+            attempts-=1
--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -5,14 +5,16 @@ EX 4_3
 Writing Tests Using pytest
 """
 import pytest
-#from guess_the_word import ...
+from guess_the_word import word_picker, words
 #from guess_the_word import ...
 
 #The selected word comes from the predefined list
 def test_predefined_list():
-    print("did stuff")
-
+    word = word_picker(words)
+    assert word in words
 
 #Guesses are processed correctly (correct vs. incorrect)
-def test_guesses_process():
-    print("did stuff")
+#def test_guesses_process():
+#    print("did stuff")
+
+#test_predefined_list(words)
